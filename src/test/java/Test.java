02a23b83d4d33db7a38599e83b184e3ad45c7673
yourname <youremail@email.com>
2017-01-09 15:06:17 +0800

    提交测试类

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -18,5 +18,8 @@ public class Test {
 //            System.out.println(rs.getRow());
 //        }
 //    }
-
+	public static void main(String args[]){
+		System.out.println(System.getProperty("user.home"));
+		System.out.println(System.getProperty("os.name"));
+	}
 }
